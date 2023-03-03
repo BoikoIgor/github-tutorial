7f2b001295bc6a88a7fb4890082c7e71b128996c
Igor Boiko <igorboiko@gmail.com>
2023-03-03 11:33:24 +0200

    add eslint config

---
+++ js/index.js
@@ -0,0 +1,84 @@
+console.log('Hello World!');
+// ----- Геттери Date ------
+// // const date = new Date();
+// // console.log('Date: ', date);
+
+// // // Повертає день місяця від 1 до 31
+// // console.log('getDate(): ', date.getDate());
+
+// // // Повертає день тижня від 0 до 6
+// // console.log('getDay(): ', date.getDay());
+
+// // // Повертає місяць від 0 до 11
+// // console.log('getMonth(): ', date.getMonth());
+
+// // // Повертає рік з 4 цифр
+// // console.log('getFullYear(): ', date.getFullYear());
+
+// // // Повертає години
+// // console.log('getHours(): ', date.getHours());
+
+// // // Повертає хвилини
+// // console.log('getMinutes(): ', date.getMinutes());
+
+// // // Повертає секунди
+// // console.log('getSeconds(): ', date.getSeconds());
+
+// // // Повертає мілісекунди
+// // console.log('getMilliseconds(): ', date.getMilliseconds());
+
+// ------ у форматі UTC (Coordinated Universal Time) ----
+// // const date = new Date();
+// // console.log('Date: ', date);
+
+// // // Повертає день місяця від 1 до 31
+// // console.log('getUTCDate(): ', date.getUTCDate());
+
+// // // Повертає день тижня від 0 до 6
+// // console.log('getUTCDay(): ', date.getUTCDay());
+
+// // // Повертає місяць від 0 до 11
+// // console.log('getUTCMonth(): ', date.getUTCMonth());
+
+// // // Повертає рік з 4 цифр
+// // console.log('getUTCFullYear(): ', date.getUTCFullYear());
+
+// // // Повертає години
+// // console.log('getUTCHours(): ', date.getUTCHours());
+
+// // // Повертає хвилини
+// // console.log('getUTCMinutes(): ', date.getUTCMinutes());
+
+// // // Повертає секунди
+// // console.log('getUTCSeconds(): ', date.getUTCSeconds());
+
+// // // Повертає мілісекунди
+// // console.log('getUTCMilliseconds(): ', date.getUTCMilliseconds());
+
+// ------Форматування дати -----
+// // const date = new Date('March 16, 2030 14:25:00');
+
+// // date.toString();
+// // // "Sat Mar 16 2030 14:25:00 GMT+0200 (Eastern European Standard Time)"
+
+// // date.toTimeString();
+// // // "14:25:00 GMT+0200 (Eastern European Standard Time)"
+
+// // date.toLocaleTimeString();
+// // // "2:25:00 PM"
+
+// // date.toUTCString();
+// // // "Sat, 16 Mar 2030 12:25:00 GMT"
+
+// // date.toDateString();
+// // // "Sat Mar 16 2030"
+
+// // date.toISOString();
+// // // "2030-03-16T12:25:00.000Z"
+
+// // date.toLocaleString();
+// // // "3/16/2030, 2:25:00 PM"
+
+// // date.getTime();
+// // // 1899894300000
+const a = 1;
